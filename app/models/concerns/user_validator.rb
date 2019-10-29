5a4c24a13418fa210d961649a3cbf25bb773b0ca
Lê Hoàng Tâm <kokoro.lehoang@gmail.com>
2019-10-29 22:47:14 +0700

    Remove validate by Proc

--- app/models/concerns/user_validator.rb
+++ app/models/concerns/user_validator.rb
@@ -6,11 +6,20 @@ module UserValidator
     p 'before remove'
     p _validators
     _validate_callbacks.each do |callback|
-      binding.pry
+
+      # for more details about callback.filter and callback.raw_filter
+      # see https://github.com/rails/rails/blob/a8dfec903bd8cd3b7df9f9d5fbb7100520049302/activesupport/lib/active_support/callbacks.rb#L306
+
+      # remove validates presence: true
       if callback.filter.is_a?(ActiveRecord::Validations::PresenceValidator) && callback.raw_filter.respond_to?(:attributes)
         _validate_callbacks.delete(callback)
       end
 
+      # remove validate by ::Proc
+      # I think dont need to check `callback.filter.is_a?(Numeric)`
+      if callback.raw_filter.is_a?(Proc) && callback.filter.is_a?(Numeric) && callback.raw_filter.respond_to?(:attributes)
+        _validate_callbacks.delete(callback)
+      end
     end
     p 'after remove'
     p _validators
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { minimum: 1, maximum: 2 }
   validates_numericality_of :age
 
+  # validate by ::Proc
+  validate do
+    errors.add(:name, 'Must be friends to leave a x') if true
+  end
+
   include UserValidator # it will override the validation declared
 end
